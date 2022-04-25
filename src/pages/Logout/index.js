d1baee3ac6fcdea749437c9344cd1b5f97b3d0a2
Carlos Miranda <carlinhostvd@icloud.com>
2022-04-24 21:01:23 -0300

    Correção hotreload

---
+++ src/pages/Logout/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ContainerPage } from "../../Componentes/Main";
+
+
+const Page = () => {
+  return (
+    <ContainerPage>
+
+    </ContainerPage>
+  );
+};
+
+export default Page;
